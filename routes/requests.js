c8970364970fe6f904659061d6fe4b8f27885e2d
yvette <ybm2@rice.edu>
2022-03-06 15:52:29 -0600

    Navbar

--- routes/requests.js
+++ routes/requests.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // Home page: Page where people make requests
 router.get('/', function(req, res) {
-    res.send('Requests form')
+    res.render('request', {page: 'Make Requests', menuId: 'home', user: req.user});
 });
 
 router.get('/view-requests', function(req, res) {
